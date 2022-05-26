6d2e34a9209deeb028382206e6e2f673a13716f4
hdowsett123 <88186800+hdowsett123@users.noreply.github.com>
2022-05-26 19:49:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -425,7 +425,7 @@ resource "aws_api_gateway_integration" "put-lambda_root" {
 resource "aws_api_gateway_deployment" "put-api" {
   depends_on = [
     aws_api_gateway_integration.put-lambda,
-    "aws_api_gateway_integration.put-lambda_root",
+    aws_api_gateway_integration.put-lambda_root,
   ]
 
   rest_api_id = aws_api_gateway_rest_api.resume-api.id
