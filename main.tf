8f8c6671fac030754e0f95f97d829637133bb533
hdowsett123 <88186800+hdowsett123@users.noreply.github.com>
2022-06-04 10:38:13 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ resource "aws_s3_bucket_object" "index_html" {
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() fu>
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("${GITHUB_ACTION_PATH}")
+  etag = filemd5("cloud-resume-challenge/website/")
 }
 
 resource "aws_s3_bucket_object" "index_css" {
@@ -67,7 +67,7 @@ resource "aws_s3_bucket_object" "index_css" {
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() fu>
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("cloud-resume-challenge/website/index.css")
+  etag = filemd5("cloud-resume-challenge/website/")
 }
 
 resource "aws_s3_bucket_object" "error_html" {
@@ -79,7 +79,7 @@ resource "aws_s3_bucket_object" "error_html" {
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() fu>
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("cloud-resume-challenge/website/error.html")
+  etag = filemd5("cloud-resume-challenge/website/")
 }
 
 resource "aws_iam_policy" "api_gw-policy" {
