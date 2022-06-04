b1f7be2be70255b53377f5508c964dd1b33b0615
hdowsett123 <88186800+hdowsett123@users.noreply.github.com>
2022-06-04 10:58:40 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,13 +48,13 @@ locals {
 resource "aws_s3_bucket_object" "index_html" {
   bucket       = var.bucket_name
   key          = "index.html"
-  source       = "cloud-resume-challenge/website/index.html"
+  source       = "cloud-resume-challenge/website"
   content_type = "text/html"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() fu>
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5(local.cloud-resume-challenge/website/index.html)
+  # etag = filemd5("cloud-resume-challenge/website/index.html")
 }
 
 resource "aws_s3_bucket_object" "index_css" {
